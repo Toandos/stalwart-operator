96bbfa5beb48c04897c376be9dee9a94d685df30
xXjojaXx <ljonny181@gmail.com>
2026-08-25 23:55:03 +0200

    feat: Init kubebuilder

---
+++ api/v1alpha1/cluster_types.go
@@ -0,0 +1,70 @@
+/*
+Copyright 2026.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+// ClusterSpec defines the desired state of Cluster
+type ClusterSpec struct {
+	// +optional
+	Foo *string `json:"foo,omitempty"`
+}
+
+// ClusterStatus defines the observed state of Cluster.
+type ClusterStatus struct {
+	// +optional
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// Cluster is the Schema for the clusters API
+type Cluster struct {
+	metav1.TypeMeta `json:",inline"`
+
+	// metadata is a standard object metadata
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitzero"`
+
+	// spec defines the desired state of Cluster
+	// +required
+	Spec ClusterSpec `json:"spec"`
+
+	// status defines the observed state of Cluster
+	// +optional
+	Status ClusterStatus `json:"status,omitzero"`
+}
+
+// +kubebuilder:object:root=true
+
+// ClusterList contains a list of Cluster
+type ClusterList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitzero"`
+	Items           []Cluster `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(func(s *runtime.Scheme) error {
+		s.AddKnownTypes(SchemeGroupVersion, &Cluster{}, &ClusterList{})
+		return nil
+	})
+}
